b81db64cda243c5867f18729fb6be02c91d33a85
superiocco <pablo_iocco@hotmail.com>
2012-03-17 19:18:27 -0300

    src

---
+++ src/com/phonegap/FacebooktestActivity.java
@@ -0,0 +1,14 @@
+package com.phonegap;
+
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class FacebooktestActivity  extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
